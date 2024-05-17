0b603d2fb874c8fb5848ad9b0b4632b3f614238f
Samiran_Das <samiran4209@gmail.com>
2024-05-17 17:43:55 +0530

    first commit

--- src/model/user.ts
+++ src/model/user.ts
@@ -4,3 +4,15 @@ export interface Message extends Document{
 content:string,
 createdAt:Date
 }
+
+const MessageSchema: Schema<Message>= new Schema({
+content:{
+    type:String,
+    required:true,
+},
+createdAt:{
+    type:Date,
+    required:true,
+    default:Date.now
+}
+})
