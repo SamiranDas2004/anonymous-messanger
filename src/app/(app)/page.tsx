ff0187ffeb6c8dbfee182cbe9b90b1605538f5cf
Samiran_Das <samiran4209@gmail.com>
2024-09-10 05:23:29 +0530

    ""

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 True Feedback. All rights reserved.
+        © 2023 True Secret message. All rights reserved.
       </footer>
     </>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Secrect Msg',
+  title: 'Secret Message',
   description: 'Nobody will no.',
 };
 
