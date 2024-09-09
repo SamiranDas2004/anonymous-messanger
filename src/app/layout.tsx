fe36cf71f19611c71c72229c0ec2f2a47b2dd906
Samiran_Das <samiran4209@gmail.com>
2024-09-10 05:18:32 +0530

    ""

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'True Feedback',
-  description: 'Real feedback from real people.',
+  title: 'Secrect Msg',
+  description: 'Nobody will no.',
 };
 
 interface RootLayoutProps {
